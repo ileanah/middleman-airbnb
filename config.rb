a8c358b579f1aea44ec7caf6ecee2067260ddf39
Ileana dHuart <ileana.dhuart@gmail.com>
2018-07-30 15:28:53 +0200

    finish

--- config.rb
+++ config.rb
@@ -20,3 +20,8 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+data.flats.each do |key, user|
+  proxy "/flats/#{key}.html", "/flats/show.html", locals: { owner: key }, ignore: true
+end
+
